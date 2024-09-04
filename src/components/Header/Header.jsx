31170735c9eabecfe085cb092888e9170f641331
RaYkeSS <raykess25rus@gmail.com>
2024-09-04 16:01:41 +0700

    Header + hero layout and styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,41 @@
+import "./Header.scss";
+
 export default function Header() {
-    return (
-        <header>
-            <div className="container">
-                <div>
-                    <a href="#"><img src="" alt="Logo" /></a>
-                </div>
-                <div>
-                    <ul>
-                        <li>Главная</li>
-                        <li>Услуги</li>
-                        <li>Этапы</li>
-                        <li>О нас</li>
-                        <li>Контакты</li>
-                    </ul>
-                </div>
-                <div>
-                    <a href="tel:+7(914)963-86-50">+7(914)963-86-50</a>
-                    <button>Обратный звонок</button>
-                    <a>Whats App</a>
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header className="header">
+      <div className="container header__container">
+        <div className="logo">
+          <a className="logo__link" href="#">
+            <img className="logo__img" src="" alt="Logo" />
+          </a>
+        </div>
+        <div>
+          <ul className="nav">
+            <li className="nav__item">
+              <a className="link nav__link">Главная</a>
+            </li>
+            <li className="nav__item">
+              <a className="link nav__link">Услуги</a>
+            </li>
+            <li className="nav__item">
+              <a className="link nav__link">Этапы</a>
+            </li>
+            <li className="nav__item">
+              <a className="link nav__link">О нас</a>
+            </li>
+            <li className="nav__item">
+              <a className="link nav__link">Контакты</a>
+            </li>
+          </ul>
+        </div>
+        <div className="feedback">
+          <a className="link feedback__link" href="tel:+7(914)963-86-50">
+            +7(914)963-86-50
+          </a>
+          <button className="btn feedback__btn">Обратный звонок</button>
+          <a className="feedback__link">Whats App</a>
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,17 @@
+import "./Hero.scss";
+
 export default function Hero() {
-    return (
-        <section>
-            <div className="container">
-                <h1>Таможенное оформление</h1>
-                <p>Оказываем услуги по таможенному оформлению на любых таможнях Российской Федерации - работаем ежедневно и при необходимости круглосуточно!</p>
-                <button>Консультация</button>
-            </div>
-        </section>
-    )
+  return (
+    <section className="hero">
+      <div className="container hero__container">
+        <h1 className="hero__title">Таможенное оформление</h1>
+        <p className="hero__text">
+          Оказываем услуги по таможенному оформлению на любых таможнях
+          Российской Федерации - работаем ежедневно и при необходимости
+          круглосуточно!
+        </p>
+        <button className="btn hero__btn">Консультация</button>
+      </div>
+    </section>
+  );
 }
